655c5e6cbf81e27e703259682a04993c47d9ecd1
Vasuishere <vaibhav60080@gmail.com>
2024-12-24 00:34:37 +0530

    settings

--- webapp/settings.py
+++ webapp/settings.py
@@ -16,6 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -26,7 +28,8 @@ SECRET_KEY = 'django-insecure-(&6+vxg$o!89$iu^uqaghje8jp$l1ej72)o)cm@dqwrf(m6%v!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+
+
 
 
 # Application definition
